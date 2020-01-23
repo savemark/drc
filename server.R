c8f7faba721adc3a59787186a1199492052964b4
Christian Savemark <mathidler@gmail.com>
2020-01-23 16:06:40 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -3,30 +3,60 @@ library(shiny)
 shinyServer(
   function(input, output) {
     
-    output$checkInput <- renderPrint({
+    # Datasets
+    datasetInterestRateSwap <- reactive({
       suppressWarnings(par_t <- as.numeric(trimws(unlist(strsplit(input$swapvector, ",")))))
       irs <- interestRateSwap(par_t, credit.adj = input$credit_adjustment, allow.negative.rates = input$allow_negative_rates)
-      print(irs)
     })
     
-    #output$checkInput <- DT::renderDT({
-    #  suppressWarnings(par_t <- as.numeric(trimws(unlist(strsplit(input$swapvector, ",")))))
-    #  irs <- interestRateSwap(par_t, credit.adj = input$credit_adjustment, allow.negative.rates = input$allow_negative_rates)
-    #  datatable(irs, options = list(paging = FALSE, info = FALSE, searching = FALSE)) %>% formatRound(columns = 2:6, digits = 5) %>% formatStyle(columns = 1:6, fontSize = "8pt")
-    #})
-    
-    output$wirs <- renderPrint({
+    datasetWeightedInterestRateSwap <- reactive({
       suppressWarnings(par_t <- as.numeric(trimws(unlist(strsplit(input$swapvector, ",")))))
       wirs <- weightedInterestSwap(par_t, T = input$maximum_duration, credit.adj = input$credit_adjustment_2, allow.negative.rates = input$allow_negative_rates_2, UFR = input$ufr)
-      print(wirs)
     })
     
+    # Tables
+    output$checkInput <- DT::renderDT({
+     datatable(datasetInterestRateSwap(), 
+               rownames = FALSE,
+               options = list(paging = FALSE, info = FALSE, searching = FALSE)) %>% formatRound(columns = 2:6, digits = 8) %>% formatStyle(columns = 0:6, fontSize = "8pt")
+    })
+    
+    output$wirs <- DT::renderDT({
+      datatable(datasetWeightedInterestRateSwap(), 
+                rownames = FALSE,
+                options = list(paging = FALSE, info = FALSE, searching = FALSE)) %>% formatRound(columns = 2:4, digits = 8) %>% formatStyle(columns = 0:6, fontSize = "8pt")
+    })
+    
+    # Plots
     output$drkPlot <- renderPlot({
+      par(bg = NA)
       suppressWarnings(par_t <- as.numeric(trimws(unlist(strsplit(input$swapvector, ",")))))
       irs <- interestRateSwap(par_t)
-      wirs <- weightedInterestSwap(par_t, T = input$maximum_duration, credit.adj = input$credit_adjustment_2, allow.negative.rates = input$allow_negative_rates_2)
-      plot(wirs[, 4], xlab = "Löptid", ylab = "Nollkupongränta", type = "l", main = "Diskonteringsräntekurva")
+      wirs <- weightedInterestSwap(par_t, T = input$maximum_duration, credit.adj = input$credit_adjustment_2, allow.negative.rates = input$allow_negative_rates_2, UFR = input$ufr)
+      plot(wirs[, 4], xlab = "Löptid", ylab = "Nollkupongränta", type = "p", main = "Diskonteringsräntekurva")
+      lines(wirs[, 4])
     })
+    
+    # Downloads
+    output$downloadDatasetInterestRateSwap <- downloadHandler(
+      filename = function() {
+        paste("irs", ".xlsx", sep = "")
+      },
+      content = function(file) {
+        xlsx::write.xlsx(datasetInterestRateSwap(), file, sheetName = "Nollkupongränta", 
+                         col.names = TRUE, row.names = FALSE, append = FALSE)
+      }
+    )
+    
+    output$downloadDatasetWeightedInterestRateSwap <- downloadHandler(
+      filename = function() {
+        paste("wirs", ".xlsx", sep = "")
+      },
+      content = function(file) {
+        xlsx::write.xlsx(datasetWeightedInterestRateSwap(), file, sheetName = "Nollkupongränta", 
+                         col.names = TRUE, row.names = FALSE, append = FALSE)
+      }
+    )
     
   }
 )
--- ui.R
+++ ui.R
@@ -6,64 +6,108 @@ source("drk.R")
 
 shinyUI(
   fluidPage(
+    tags$head(
+      tags$style(HTML(
+      "
+      #checkInput table > thead > tr > th,
+      #checkInput table > tbody > tr > th,
+      #checkInput table > tfoot > tr > th,
+      #checkInput table > thead > tr > td,
+      #checkInput table > tbody > tr > td,
+      #checkInput table > tfoot > tr > td {padding:0px;}"
+      )
+      )
+    ),
+    tags$head(
+      tags$style(HTML(
+      "
+      #wirs table > thead > tr > th,
+      #wirs table > tbody > tr > th,
+      #wirs table > tfoot > tr > th,
+      #wirs table > thead > tr > td,
+      #wirs table > tbody > tr > td,
+      #wirs table > tfoot > tr > td {padding:0px;}"
+      )
+      )
+    ),
     theme = shinytheme("simplex"),
-    titlePanel("Diskonteringsräntekurvor"),
-    p("Detta program beräknar diskonteringsräntekurvor både från idag och framåt i tiden,
+    fluidRow(
+      column(8,
+             titlePanel("Diskonteringsräntekurvor"),
+             p("Detta program beräknar diskonteringsräntekurvor både från idag och framåt i tiden,
                     för exempelvis användning till att beräkna värdet av framtida balansräkningar. Kurvorna kan enkelt skrivas till 
                     Excel i tabellformat.\n
                     Indata är marknadsnoterade swapräntor."),
-    p("Se beräkningsmetodik i PROMEMORIA 2013-12-01 [FI Dnr 13-11409]."),
-    hr(),
+             p("Se beräkningsmetodik i PROMEMORIA 2013-12-01 [FI Dnr 13-11409]."),
+             offset = 1)
+    ),
+    br(),
     fluidRow(
-      column(6,
-             h4("1. Oviktad diskonteringskurva"),
-             textInput("swapvector", "Skriv in marknadsnoteringar för swapräntor", "-0.00095, -0.001, -0.00103, -0.00083, -0.00045, 0.00005, 0.00065, 0.00125, 0.00185, 0.00243, NA, 0.00348, NA, NA, 0.00465, NA, NA, NA, NA, 0.00578", width = "100%"),
+      column(8,
+             h4("MARKNADSNOTERINGAR FÖR SWAPRÄNTOR"),
+             textInput("swapvector", "Marknadsnoteringar", "-0.00095, -0.001, -0.00103, -0.00083, -0.00045, 0.00005, 0.00065, 0.00125, 0.00185, 0.00243, NA, 0.00348, NA, NA, 0.00465, NA, NA, NA, NA, 0.00578", 
+                       width = "100%"),
+             p("Skriv in marknadsnoteringar för swapräntor. NA står för Not Available. NA behöver skrivas in för noteringar som saknas om man vill ha exempelvis 20 eller 30 nollkupongräntor. Den
+               första och den sista koordinaten i n-tupeln ovan får inte vara NA eftersom diskonteringsfaktorer beräknas rekursivt mellan kända noteringar."),
+             hr(),
+             offset = 1
+      )
+    ),
+    fluidRow(
+      column(8,
+             h4("1. DISKONTERINGSRÄNTEKURVA FRÅN MARKNADSNOTERADE SWAPRÄNTOR"),
              fluidRow(
-               column(6,
-                      numericInput("credit_adjustment", "Kreditjustering", -0.0035)
+               column(2,
+                      numericInput("credit_adjustment", "Kreditriskjustering", -0.0035)
                ),
-               column(6,
+               column(2,
                       checkboxInput("allow_negative_rates", "Tillåt negativa räntor", value = TRUE)
                )
              ),
-             offset = 3
+             offset = 1
       )
     ),
     fluidRow(
-      column(6,
-             "Kontroll:",
-             verbatimTextOutput("checkInput"),
+      column(8,
+             DTOutput("checkInput"),
              hr(),
-             offset = 3
+             offset = 1
+      ),
+      column(2,
+             downloadButton("downloadDatasetInterestRateSwap", "Ladda ned")
       )
     ),
     fluidRow(
-      column(6,
-             h4("2. Viktad diskonteringskurva"),
+      column(8,
+             h4("2. LINJÄRT VIKTADE TERMINSRÄNTOR"),
              fluidRow(
-               column(3,
-                      numericInput("credit_adjustment_2", "Kreditjustering", -0.0035)
-               ),
-               column(3,
-                      numericInput("ufr", "UFR", 0.042)
-               ),
-               column(3,
-                      sliderInput("maximum_duration", "Längsta löptid", min = 1, max = 150, value = 50)
-                      ),
-               column(3,
-                      checkboxInput("allow_negative_rates_2", "Tillåt negativa räntor", value = TRUE)
+               column(8,
+                      plotOutput("drkPlot")
                )
              ),
-             offset = 3
-      )
-    ),
-    fluidRow(
-      column(6,
-             verbatimTextOutput("wirs"),
-             offset = 3
-      ),
-      column(3,
-             plotOutput("drkPlot")
+             fluidRow(
+               column(2,
+                      numericInput("credit_adjustment_2", "Kreditriskjustering", -0.0035)
+               ),
+               column(2,
+                      numericInput("ufr", "UFR", 0.042)
+               ),
+               column(2,
+                      checkboxInput("allow_negative_rates_2", "Tillåt negativa räntor", value = TRUE)
+               ),
+               column(2,
+                      sliderInput("maximum_duration", "Längsta löptid", min = 1, max = 150, value = 120)
+               )
+             ),
+             fluidRow(
+               column(8,
+                      DTOutput("wirs")
+               ),
+               column(2,
+                      downloadButton("downloadDatasetWeightedInterestRateSwap", "Ladda ned")
+               )
+             ),
+             offset = 1
       )
     )
   )
