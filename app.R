f50fb3827418bc02a2aa0fc5d045c8bb420d221a
Christian Savemark <mathidler@gmail.com>
2020-02-02 16:52:16 +0100

    Update app.R

--- app.R
+++ app.R
@@ -12,12 +12,23 @@ ui <- fluidPage(
   tags$head(
     tags$style(HTML(
       "
-      #name-wirs table > thead > tr > th,
-      #name-wirs table > tbody > tr > th,
-      #name-wirs table > tfoot > tr > th,
-      #name-wirs table > thead > tr > td,
-      #name-wirs table > tbody > tr > td,
-      #name-wirs table > tfoot > tr > td {padding:0px;}")
+      #none-wirs table > thead > tr > th,
+      #none-wirs table > tbody > tr > th,
+      #none-wirs table > tfoot > tr > th,
+      #none-wirs table > thead > tr > td,
+      #none-wirs table > tbody > tr > td,
+      #none-wirs table > tfoot > tr > td {padding:0px;}")
+    )
+  ),
+  tags$head(
+    tags$style(HTML(
+      "
+      #abs_up-wirs table > thead > tr > th,
+      #abs_up-wirs table > tbody > tr > th,
+      #abs_up-wirs table > tfoot > tr > th,
+      #abs_up-wirs table > thead > tr > td,
+      #abs_up-wirs table > tbody > tr > td,
+      #abs_up-wirs table > tfoot > tr > td {padding:0px;}")
     )
   ),
   theme = shinytheme("simplex"),
@@ -28,10 +39,10 @@ ui <- fluidPage(
                     för exempelvis användning till att beräkna värdet av framtida balansräkningar. Kurvorna kan enkelt skrivas till 
                     Excel i tabellformat.\n
                     Indata är marknadsnoterade swapräntor."),
-           p("Se beräkningsmetodik i PROMEMORIA 2013-12-01 [FI Dnr 13-11409].")
+           p("Se beräkningsmetodik i PROMEMORIA 2013-12-01 [FI Dnr 13-11409]."),
+           offset = 1
     )
   ),
-  br(),
   fluidRow(
     column(8,
            h4("MARKNADSNOTERINGAR FÖR SWAPRÄNTOR"),
@@ -40,7 +51,8 @@ ui <- fluidPage(
            # -0.00095, -0.001, -0.00103, -0.00083, -0.00045, 0.00005, 0.00065, 0.00125, 0.00185, 0.00243, NA, 0.00348, NA, NA, 0.00465, NA, NA, NA, NA, 0.00578
            p("Skriv in marknadsnoteringar för swapräntor. NA står för Not Available. NA behöver skrivas in för noteringar som saknas om man vill ha exempelvis 20 eller 30 nollkupongräntor. Den
                första och den sista koordinaten i n-tupeln ovan får inte vara NA eftersom diskonteringsfaktorer beräknas rekursivt mellan kända noteringar."),
-           hr()
+           hr(),
+           offset = 1
     )
   ),
   fluidRow(
@@ -48,19 +60,35 @@ ui <- fluidPage(
            mainPanel(
              tabsetPanel(
                type = "tabs",
-               tabPanel("Ostressad", weightedInterestRateSwapUI("name")),
-               tabPanel("Stressad", weightedInterestRateSwapUI("name2"))
+               tabPanel("Ostressad", weightedInterestRateSwapUI("none")),
+               tabPanel("Stressad: Räntehöjningschock (absolut)", 
+                        weightedInterestRateSwapUI("abs_up", 
+                                                   selected = list(stress_type = "absolute_up"))),
+               tabPanel("Stressad: Räntesänkningschock (absolut)", 
+                        weightedInterestRateSwapUI("abs_down", 
+                                                   selected = list(stress_type = "absolute_down"))),
+               tabPanel("Stressad: Räntehöjningschock (relativ)", 
+                        weightedInterestRateSwapUI("rel_up", 
+                                                   selected = list(stress_type = "relative_up"))),
+               tabPanel("Stressad: Räntesänkningschock (relativ)", 
+                        weightedInterestRateSwapUI("rel_down",
+                                                   selected = list(stress_type = "relative_down")))
              ),
              width = 12
-           )
+           ),
+           offset = 1
     )
   )
 )
 
 # Server logic
 server <- function(input, output, session) {
-  callModule(weightedInterestRateSwapServer, "name", swaprate = input$swaprate)
-  callModule(weightedInterestRateSwapServer, "name2", swaprate = input$swaprate)
+  sr <- reactive({input$swaprate})
+  callModule(weightedInterestRateSwapServer, "none", swaprate = sr)
+  callModule(weightedInterestRateSwapServer, "abs_up", swaprate = sr)
+  callModule(weightedInterestRateSwapServer, "abs_down", swaprate = sr)
+  callModule(weightedInterestRateSwapServer, "rel_up", swaprate = sr)
+  callModule(weightedInterestRateSwapServer, "rel_down", swaprate = sr)
 }
 
 shinyApp(ui, server)
