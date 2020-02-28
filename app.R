136bbb9de4de18835493036fc8b97c9b0d717541
Christian Savemark <mathidler@gmail.com>
2020-02-28 15:57:35 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,9 +1,11 @@
 # Load packages
 library(shiny)
-#library(shinyWidgets)
+library(shinyWidgets)
 library(shinydashboard)
 library(shinydashboardPlus)
+library(shinyjs)
 library(DT)
+library(wesanderson)
 
 # Load separate module and function scripts
 source("functions_drc.R")
@@ -12,16 +14,23 @@ source("module_drc.R")
 # User interface
 ui <- dashboardPagePlus(
   skin = "black-light",
+  sidebar_fullCollapse = TRUE, 
+  enable_preloader = FALSE,
+  loading_duration = 1,
+  useShinyjs(),
   header = dashboardHeaderPlus(
-    title = "Verktyg",
+    title = tags$div(a(href = 'http://www.fi.se',
+                      img(src = "fi-logotyp.png")), 
+                      class = "dropdown"),
     enable_rightsidebar = TRUE,
-    rightSidebarIcon = "info-circle"
+    rightSidebarIcon = "gears"
   ),
   sidebar = dashboardSidebar(
     sidebarMenu(
       menuItem("Räntekurvsberäkningar", tabName = "tab_module_drc", icon = icon("calculator", class= "fas", lib = "font-awesome")),
       menuItem("Solvensmodellen", tabName = "tab_module_solvencymod", icon = icon("calculator", class= "fas", lib = "font-awesome")),
-      menuItem("ESG-Analysverktyg", tabName = "tab_module_esg", icon = icon("calculator", class= "fas", lib = "font-awesome"))
+      menuItem("ESG-Analysverktyg", tabName = "tab_module_esg", icon = icon("calculator", class= "fas", lib = "font-awesome")),
+      id = "nav"
     )
   ),
   body = dashboardBody(
@@ -34,7 +43,7 @@ ui <- dashboardPagePlus(
                   icon = "fa fa-th",
                   gradientColor = "maroon", 
                   boxToolSize = "xs", 
-                  width = 10,
+                  width = 12,
                   collapsible = TRUE,
                   closable = TRUE,
                   p("Detta program beräknar diskonteringsräntekurvor både från idag och framåt i tiden,
@@ -48,7 +57,7 @@ ui <- dashboardPagePlus(
                 box(
                   title = "Marknadsnoteringar för swapräntor", 
                   status = "warning",
-                  width = 10,
+                  width = 12,
                   collapsible = TRUE,
                   textInput("swaprate", "Marknadsnoteringar", "-0.00095, -0.001, -0.00103, -0.00083, -0.00045, 0.00005, 0.00065, 0.00125, 0.00185, 0.00243, NA, 0.00348, NA, NA, 0.00465, NA, NA, NA, NA, 0.00578", 
                             width = "100%"),
@@ -61,16 +70,16 @@ ui <- dashboardPagePlus(
                 box(
                   title = "Figur",
                   status = "info",
-                  width = 10,
+                  width = 12,
                   collapsible = TRUE,
                   plotOutput("zcr_curve_all")
                 )
               ),
               fluidRow(
                 box(
-                  title = "Modul",
+                  title = "Tabeller",
                   status = "primary",
-                  width = 10,
+                  width = 12,
                   collapsible = TRUE,
                   mainPanel(
                     tabsetPanel(
@@ -101,19 +110,11 @@ ui <- dashboardPagePlus(
     )
   ),
   rightsidebar = rightSidebar(
-    background = "dark",
-    title = "Sidebar",
-    rightSidebarTabContent(
-      id = 1,
-      icon = "desktop",
-      title = "Tab 1",
-      active = TRUE,
-      sliderInput(
-        "obs",
-        "Number of observations:",
-        min = 0, max = 1000, value = 500
-      )
-    )
+    tags$script(HTML(
+      "$(\".control-sidebar\").on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() { $(window).trigger(\"resize\"); } );"
+    )),
+    uiOutput("right_side_bar"),
+    background = "light"
   ),
   footer = dashboardFooter(
     
@@ -122,23 +123,140 @@ ui <- dashboardPagePlus(
 
 # Server logic
 server <- function(input, output, session) {
-  sr <- reactive({input$swaprate})
-  
-  # Modules
-  wirss_none <- callModule(weightedInterestRateSwapServer, "none", swaprate = sr)
-  wirss_abs_up <- callModule(weightedInterestRateSwapServer, "abs_up", swaprate = sr)
-  wirss_abs_down <- callModule(weightedInterestRateSwapServer, "abs_down", swaprate = sr)
-  wirss_rel_up <- callModule(weightedInterestRateSwapServer, "rel_up", swaprate = sr)
-  wirss_rel_down <- callModule(weightedInterestRateSwapServer, "rel_down", swaprate = sr)
-  
-  # Plots
-  output$zcr_curve_all <- renderPlot({
-    par(bg = NA)
-    data <- wirss_none$wirs()
-    plot(data[, 8], xlab = "L\u00F6ptid", ylab = "Nollkupongr\u00E4nta", type = "p", main = "Diskonteringsr\u00E4ntekurvor")
-    lines(data[, 8])
-    grid(lty = "dotted")
+  observe({
+    if (req(input$nav) == "tab_module_drc"){
+      shinyjs::addClass(selector = "body", class = "control-sidebar-open")
+      output$right_side_bar <- renderUI({
+        rightSidebarTabContent(
+          id = "T_tab_module_drc",
+          icon = "desktop",
+          title = "Inställningar",
+          p("Argument för diskonteringsräntekurvorna"),
+          numericInput("credit_risk_adjustment", "Kreditriskjustering", -0.0035, step = 1/10000),
+          numericInput("ufr", "Ultimate Forward Rate", 0.042, step = 1/10000),
+          checkboxInput("allow_negative_rates", "Till\u00E5t negativa kreditriskjusterade swapr\u00E4ntor", value = TRUE),
+          sliderInput("maximum_maturity", "L\u00E4ngsta l\u00F6ptid", min = 1, max = 150, value = 120),
+          downloadButton("downloadDatasetWeightedInterestRateSwap", "Ladda ned tabeller")
+        )
+      })
+      
+      # Modules
+      wirss_none <- callModule(weightedInterestRateSwapServer, 
+                               "none", 
+                               swaprate = reactive({input$swaprate}), 
+                               maximum_maturity = reactive({input$maximum_maturity}), 
+                               credit_risk_adjustment = reactive({input$credit_risk_adjustment}),
+                               allow_negative_rates = reactive({input$allow_negative_rates}),
+                               ufr = reactive({input$ufr})
+      )
+      wirss_abs_up <- callModule(weightedInterestRateSwapServer, 
+                                 "abs_up", 
+                                 swaprate = reactive({input$swaprate}), 
+                                 maximum_maturity = reactive({input$maximum_maturity}),
+                                 credit_risk_adjustment = reactive({input$credit_risk_adjustment}),
+                                 allow_negative_rates = reactive({input$allow_negative_rates}),
+                                 ufr = reactive({input$ufr})
+      )
+      wirss_abs_down <- callModule(weightedInterestRateSwapServer, 
+                                   "abs_down", 
+                                   swaprate = reactive({input$swaprate}), 
+                                   maximum_maturity = reactive({input$maximum_maturity}),
+                                   credit_risk_adjustment = reactive({input$credit_risk_adjustment}),
+                                   allow_negative_rates = reactive({input$allow_negative_rates}),
+                                   ufr = reactive({input$ufr})
+      )
+      wirss_rel_up <- callModule(weightedInterestRateSwapServer, 
+                                 "rel_up", 
+                                 swaprate = reactive({input$swaprate}), 
+                                 maximum_maturity = reactive({input$maximum_maturity}),
+                                 credit_risk_adjustment = reactive({input$credit_risk_adjustment}),
+                                 allow_negative_rates = reactive({input$allow_negative_rates}),
+                                 ufr = reactive({input$ufr})
+      )
+      wirss_rel_down <- callModule(weightedInterestRateSwapServer, 
+                                   "rel_down", 
+                                   swaprate = reactive({input$swaprate}), 
+                                   maximum_maturity = reactive({input$maximum_maturity}),
+                                   credit_risk_adjustment = reactive({input$credit_risk_adjustment}),
+                                   allow_negative_rates = reactive({input$allow_negative_rates}),
+                                   ufr = reactive({input$ufr})
+      )
+      
+      # Plots
+      output$zcr_curve_all <- renderPlot({
+        par(bg = NA)
+        data <- cbind(wirss_none$wirs()[ , 8], wirss_abs_up$wirs()[ , 8], wirss_abs_down$wirs()[ , 8], wirss_rel_up$wirs()[ , 8], wirss_rel_down$wirs()[ , 8])
+        plot(data[, 1], xlab = "L\u00F6ptid", ylab = "Nollkupongr\u00E4nta", type = "p", main = "Diskonteringsr\u00E4ntekurvor", ylim = 1.10*c(min(data), max(data)))
+        lapply(1:5, function(x) {lines(data[ , x], col = wes_palette("Zissou1", 5)[x])})
+        lapply(1:5, function(x) {points(data[ , x], col = wes_palette("Zissou1", 5)[x])})
+        legend("topleft", 
+               legend = c("Ostressad", 
+                          "Absolut räntehöjningschock",
+                          "Absolut räntesänkningschock",
+                          "Relativ räntehöjningschock",
+                          "Relativ räntesänkningschock"),
+               border = "NA",
+               col = wes_palette("Zissou1", 5),
+               bty = "n",
+               lty = 1,
+               pch = 1,
+               bg = "white",
+               inset = 0.01
+        )
+        grid(lty = "dotted", lwd = 0.5)
+      })
+      
+      # Downloads
+      output$downloadDatasetWeightedInterestRateSwap <- downloadHandler(
+        filename = function() {
+          paste("wirs", ".xlsx", sep = "")
+        },
+        content = function(file) {
+          xlsx::write.xlsx(wirss_none$wirs(), 
+                           file, 
+                           sheetName = "Ostressad", 
+                           col.names = TRUE, 
+                           row.names = FALSE, 
+                           append = FALSE)
+          xlsx::write.xlsx(wirss_abs_up$wirs(), 
+                           file, 
+                           sheetName = "Stress Absolut upp", 
+                           col.names = TRUE, 
+                           row.names = FALSE, 
+                           append = TRUE)
+          xlsx::write.xlsx(wirss_abs_down$wirs(), 
+                           file, 
+                           sheetName = "Stress Absolut ned", 
+                           col.names = TRUE, 
+                           row.names = FALSE, 
+                           append = TRUE)
+          xlsx::write.xlsx(wirss_rel_up$wirs(), 
+                           file, 
+                           sheetName = "Stress Relativ upp", 
+                           col.names = TRUE, 
+                           row.names = FALSE, 
+                           append = TRUE)
+          xlsx::write.xlsx(wirss_rel_down$wirs(), 
+                           file, 
+                           sheetName = "Stress Relativ ned", 
+                           col.names = TRUE, 
+                           row.names = FALSE, 
+                           append = TRUE)
+        }
+      )
+      
+    }
+    if (req(input$nav) == "tab_module_solvencymod"){
+      shinyjs::removeClass(selector = "body", class = "control-sidebar-open")
+      output$right_side_bar <- renderUI({ div() })
+    }
+    if (req(input$nav) == "tab_module_esg"){
+      shinyjs::removeClass(selector = "body", class = "control-sidebar-open")
+      output$right_side_bar <- renderUI({ div() })
+    }
   })
+  
+
 }
 
 shinyApp(ui, server)
--- module_drc.R
+++ module_drc.R
@@ -5,25 +5,6 @@ weightedInterestRateSwapUI <- function(id, selected = list()) {
   tagList(
     fluidRow(
       column(12,
-             plotOutput(ns("wdrcplot"))
-      )
-    ),
-    fluidRow(
-      column(3,
-             numericInput(ns("credit_adjustment"), "Kreditriskjustering", -0.0035, step = 1/10000)
-      ),
-      column(3,
-             numericInput(ns("ufr"), "Ultimate forward rate", 0.042, step = 1/10000)
-      ),
-      column(3,
-             checkboxInput(ns("allow_negative_rates"), "Till\u00E5t negativa kreditriskjusterade swapr\u00E4ntor", value = TRUE)
-      ),
-      column(3,
-             sliderInput(ns("maximum_maturity"), "L\u00E4ngsta l\u00F6ptid", min = 1, max = 150, value = 120)
-      )
-    ),
-    fluidRow(
-      column(9,
              radioButtons(ns("stress_type"), 
                           label = "Stresstyp:",
                           choiceNames = c("Ingen",
@@ -39,9 +20,6 @@ weightedInterestRateSwapUI <- function(id, selected = list()) {
                           selected = stress_type,
                           inline = TRUE
              )
-      ),
-      column(3,
-             downloadButton(ns("downloadDatasetWeightedInterestRateSwap"), "Ladda ned tabell")
       )
     ),
     fluidRow(
@@ -53,16 +31,23 @@ weightedInterestRateSwapUI <- function(id, selected = list()) {
 }
 
 # Module server logic
-weightedInterestRateSwapServer <- function(input, output, session, swaprate) {
+weightedInterestRateSwapServer <- function(input, 
+                                           output, 
+                                           session, 
+                                           swaprate,
+                                           maximum_maturity,
+                                           credit_risk_adjustment,
+                                           allow_negative_rates,
+                                           ufr) {
   
   # Datasets
   datasetWeightedInterestRateSwap <- reactive({
     suppressWarnings(par_t <- as.numeric(trimws(unlist(strsplit(swaprate(), ",")))))
     wirs <- weightedInterestSwap(par_t, 
-                                 T = input$maximum_maturity, 
-                                 credit.adj = input$credit_adjustment, 
-                                 allow.negative.rates = input$allow_negative_rates, 
-                                 UFR = input$ufr,
+                                 T = maximum_maturity(), 
+                                 credit.adj = credit_risk_adjustment(), 
+                                 allow.negative.rates = allow_negative_rates(), 
+                                 UFR = ufr(),
                                  stress = stress,
                                  args.stress = list(type = input$stress_type))
   })
@@ -73,34 +58,6 @@ weightedInterestRateSwapServer <- function(input, output, session, swaprate) {
               rownames = FALSE,
               options = list(paging = FALSE, info = FALSE, searching = FALSE)) %>% formatRound(columns = 2:8, digits = 8) %>% formatStyle(columns = 1:8, fontSize = "8pt")
   })
-  
-  # Plots
-  output$wdrcplot <- renderPlot({
-    par(bg = NA)
-    suppressWarnings(par_t <- as.numeric(trimws(unlist(strsplit(swaprate(), ",")))))
-    irs <- interestRateSwap(par_t)
-    wirs <- weightedInterestSwap(par_t, 
-                                 T = input$maximum_maturity, 
-                                 credit.adj = input$credit_adjustment, 
-                                 allow.negative.rates = input$allow_negative_rates, 
-                                 UFR = input$ufr,
-                                 stress = stress,
-                                 args.stress = list(type = input$stress_type))
-    plot(wirs[, 8], xlab = "L\u00F6ptid", ylab = "Nollkupongr\u00E4nta", type = "p", main = "Diskonteringsr\u00E4ntekurva")
-    lines(wirs[, 8])
-    grid(lty = "dotted")
-  })
-  
-  # Downloads
-  output$downloadDatasetWeightedInterestRateSwap <- downloadHandler(
-    filename = function() {
-      paste("wirs", ".xlsx", sep = "")
-    },
-    content = function(file) {
-      xlsx::write.xlsx(datasetWeightedInterestRateSwap(), file, sheetName = "Nollkupongränta", 
-                       col.names = TRUE, row.names = FALSE, append = FALSE)
-    }
-  )
   
   return(list(wirs = reactive({datasetWeightedInterestRateSwap()})))
 }
